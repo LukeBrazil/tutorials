b2b7e46301202c538e8e59ed096ce0f2cbc142fa
LukeBrazil <luke.brazil13@icloud.com>
2021-03-22 21:45:32 -0500

    added jwt sign

--- jwt_auth_tutorial/server.js
+++ jwt_auth_tutorial/server.js
@@ -1,12 +1,15 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
 const users = [
     {
-        name: "John"
+        username: "John"
     }
 ]
 
@@ -14,36 +17,42 @@ app.get('/users', (req, res) => {
     res.json(users)
 })
 
+
+
 app.post('/users', async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         console.log(hashedPassword)
-        const user = { name: req.body.name, password: hashedPassword }
+        const user = { username: req.body.username, password: hashedPassword }
         users.push(user)
         console.log(user)
         res.json({ message: 'USER ADDED' })
     } catch {
         res.status(500).json({ message: 'DIDNT WORK' })
     }
-    
+
 
 })
 
-app.post('/users/login', async (req, res) => {
-    const user = users.find( user => user.name === req.body.name )
+
+app.post('/login', async (req, res) => {
+    const user = users.find(user => user.username === req.body.username)
     if (user == null) {
         return res.status(400).send('Cannot find user!')
     }
 
     try {
-        if(await bcrypt.compare(req.body.password, user.password)) {
-            res.send('Success')
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            const userForToken = { username: user.username }
+            const accessToken = jwt.sign(userForToken, process.env.ACCESS_TOKEN_SECRET)
+            res.json({ accessToken: accessToken, user: user })
         } else {
             res.send('Not allowed!')
         }
     } catch {
         res.status(500).send('DIDNT WORK BRUH')
     }
+
 })
 
 
