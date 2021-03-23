00dfa89a0c47e5803e7bf04a310a08c774242ba2
LukeBrazil <luke.brazil13@icloud.com>
2021-03-22 21:19:26 -0500

    step 1

---
+++ jwt_auth_tutorial/README.md
@@ -0,0 +1,1 @@
+Link: https://www.youtube.com/watch?v=mbsmsi7l3r4
---
+++ jwt_auth_tutorial/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express()
+const bcrypt = require('bcrypt')
+
+app.use(express.json())
+
+const users = [
+    {
+        name: "John"
+    }
+]
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        console.log(hashedPassword)
+        const user = { name: req.body.name, password: hashedPassword }
+        users.push(user)
+        console.log(user)
+        res.json({ message: 'USER ADDED' })
+    } catch {
+        res.status(500).json({ message: 'DIDNT WORK' })
+    }
+    
+
+})
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find( user => user.name === req.body.name )
+    if (user == null) {
+        return res.status(400).send('Cannot find user!')
+    }
+
+    try {
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success')
+        } else {
+            res.send('Not allowed!')
+        }
+    } catch {
+        res.status(500).send('DIDNT WORK BRUH')
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log('server running...')
+})
